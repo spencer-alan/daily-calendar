f1cce6d3ea4dde957f24484f1a15c1e7d3833211
b1acksmyth <smith.spence13@gmail.com>
2020-07-10 17:17:23 -0700

    updated jQuery CDN link, started working on JS variables

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,13 @@
-$(document).ready(function){
+$(document).ready(function () {
 
-
-
-
-
-
-
-
-
-
-  
-}
+let note1 = $('#note1');
+let note2 = $('#note2');
+let note3 = $('#note3');
+let note4 = $('#note4');
+let note5 = $('#note5');
+let note6 = $('#note6');
+let note7 = $('#note7');
+let note8 = $('#note8');
+let note9 = $('#note9');
+let notes = [note1, note2, note3, note4, note5, note6, note7, note8, note9];
+});
