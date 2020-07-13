2e8824386a8be53a82a460d64d5764daea59f467
b1acksmyth <smith.spence13@gmail.com>
2020-07-12 17:01:12 -0700

    for loop to add background color to each time block dependent on time

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
   // Current date and time holder
-  let current = $('#current-date');
+  let current = $("#current-date");
   // Grabbing Areas that need to be changed dependent on time
-  let note1 = $('#note1');
-  let note2 = $('#note2');
-  let note3 = $('#note3');
-  let note4 = $('#note4');
-  let note5 = $('#note5');
-  let note6 = $('#note6');
-  let note7 = $('#note7');
-  let note8 = $('#note8');
-  let note9 = $('#note9');
+  let note1 = $("#note1");
+  let note2 = $("#note2");
+  let note3 = $("#note3");
+  let note4 = $("#note4");
+  let note5 = $("#note5");
+  let note6 = $("#note6");
+  let note7 = $("#note7");
+  let note8 = $("#note8");
+  let note9 = $("#note9");
   let notes = [note1, note2, note3, note4, note5, note6, note7, note8, note9];
   
   // Current Date and Time
@@ -25,12 +25,24 @@ $(document).ready(function () {
   let fifthHour = moment(13, "HH");
   let sixthHour = moment(14, "HH");
   let seventhHour = moment(15, "HH");
-  let eightHour = moment(16, "HH");
+  let eighthHour = moment(16, "HH");
   let ninthHour = moment(17, "HH");
-  let hours = [firstHour, secondHour, thirdHour, fourthHour, fifthHour, sixthHour, seventhHour, eightHour, ninthHour];
-  if (m.isAfter(ninthHour) === false) {
-    note1.css("background", "grey");
+  let hours = [firstHour, secondHour, thirdHour, fourthHour, fifthHour, sixthHour, seventhHour, eighthHour, ninthHour];
+  
+  for (let i = 0; i < hours.length; i++) {
+    if (m.isBefore(hours[i], "hour") === true) {
+      notes[i].css("background", "#b3ffb3");
+    }
+    else if (m.isAfter(hours[i], "hour") === true) {
+      notes[i].css("background", "#cccccc");
+    }
+    else if (m.isSame(hours[i], "hour") === true) {
+      notes[i].css("background", "#668cff");
+    }
+    console.log(i);
   }
-  console.log(m.isAfter(fifthHour));
-  console.log(m.isAfter(ninthHour));
+  
+  // console.log("Same:", m.isSame(firstHour, "hour"));
+  // console.log("After:", m.isAfter(firstHour, "hour"));
+  // console.log("Before:", m.isBefore(firstHour, "hour"));
 });
