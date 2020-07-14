dc0a5b596a963e1890afa04d2dce9aa2c1c89bf0
b1acksmyth <smith.spence13@gmail.com>
2020-07-13 22:05:58 -0700

    started work on save button

--- assets/script.js
+++ assets/script.js
@@ -12,12 +12,13 @@ $(document).ready(function () {
   let note8 = $("#note8");
   let note9 = $("#note9");
   let notes = [note1, note2, note3, note4, note5, note6, note7, note8, note9];
-  
+  let date = null;
+
   // Current Date and Time
   const m = moment();
   console.log(m.toString());
 
-  $(current).text(m.format("[Today is] dddd, MMMM Do, YYYY, h:mm a"));
+  // All the moments to update what hour you are in for the work day
   let firstHour = moment(9, "HH");
   let secondHour = moment(10, "HH");
   let thirdHour = moment(11, "HH");
@@ -29,20 +30,32 @@ $(document).ready(function () {
   let ninthHour = moment(17, "HH");
   let hours = [firstHour, secondHour, thirdHour, fourthHour, fifthHour, sixthHour, seventhHour, eighthHour, ninthHour];
   
+  // Puts the current time on at the top of the page
+  // Updates the current time by seconds
+    date = moment(new Date())
+    current.html(date.format("[Today is] dddd"));
+
   for (let i = 0; i < hours.length; i++) {
     if (m.isBefore(hours[i], "hour") === true) {
-      notes[i].css("background", "#b3ffb3");
+      notes[i].css("background", "#77dd77");
     }
     else if (m.isAfter(hours[i], "hour") === true) {
-      notes[i].css("background", "#cccccc");
+      notes[i].css("background", "#d3d3d3");
     }
     else if (m.isSame(hours[i], "hour") === true) {
-      notes[i].css("background", "#668cff");
+      notes[i].css("background", "#ff6961");
     }
-    console.log(i);
-  }
-  
+  };
   // console.log("Same:", m.isSame(firstHour, "hour"));
   // console.log("After:", m.isAfter(firstHour, "hour"));
   // console.log("Before:", m.isBefore(firstHour, "hour"));
+  
+  $(".btn-save").on("click", save);
+
+  const arrayId = [];
+
+  function save() {
+    let uniqueID = $(this).attr("data");
+    console.log(uniqueID);
+  };
 });
